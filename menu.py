0a114cdf2dd1cf1baad50fa5e73a35973e6cfbb5
Viki Mazurova <98098663+vmazurova@users.noreply.github.com>
2024-02-18 18:22:00 +0100

    commit2
    commit2

--- menu.py
+++ menu.py
@@ -1,6 +1,15 @@
 import pandas as pd
-import main
 import temp
+import temp
+
+
+def load_data():
+    data_path = 'data/klementinum.xlsx'
+    data_sheet_name = 'data'
+    temperature_data = pd.read_excel(data_path, sheet_name=data_sheet_name)
+    return temperature_data
+
+
 def menu():
     print("1 - Zobrazit průměrnou teplotu pro zadaný rok")
     print("2 - Zobrazit minimální a maximální teplotu pro zadaný rok")
@@ -15,32 +24,69 @@ def menu():
     vstup = input("Zadej akci:").upper()
     return vstup
 
+
 def zvol_akci():
+    data = load_data()
+    analytik = temp.WeatherStatistics(data)
     while True:
         vyber = menu()
         if vyber == "1":
-            temp.TemperatureAnalytics.get_average_temperature()
-        if vyber == "2":
+            while True:
+                try:
+                    rok = int(input("Zadejte rok: "))
+                    prumerna_teplota = analytik.average_temperature(rok)
+                    print(f"Průměrná teplota v roce {rok} je: {prumerna_teplota}°C")
+                    break
+                except ValueError:
+                    print("Nebylo zadano platne cislo, zadej znova")
+                    break
+
+        elif vyber == "2":
+            while True:
+                try:
+                    rok = int(input("Zadej rok pro vypocet:"))
+                    max_temp = analytik.highest_temperature(rok)
+                    min_temp = analytik.lowest_temperature(rok)
+                    print(f"Maximální teplota v roce {rok}: {max_temp}°C")
+                    print(f"Minimální teplota v roce {rok}: {min_temp}°C")
+                    break
+                except ValueError:
+                    print("Nebylo zadano platne cislo, zadej znova")
+                    break
+
+        elif vyber == "3":
+            try:
+                rok = int(input("Zadejte rok pro zobrazení měsíčních průměrů: "))
+                monthly_avg = analytik.monthly_averages(rok)
+                print(f"Měsíční průměrné teploty v roce {rok}:\n{monthly_avg}")
+                break
+            except ValueError:
+                print("Nebylo zadáno platne cislo, napis znova.")
+                break
+
+        elif vyber == "4":
+            try:
+                start_year = int(input("Zadejte počáteční rok: "))
+                end_year = int(input("Zadejte koncový rok: "))
+                trends = analytik.temperature_trends(start_year, end_year)
+                print(f"Teplotní trendy od roku {start_year} do roku {end_year}:\n{trends}")
+                break
+            except ValueError:
+                print("Nebylo zadáno platne cislo, napis znova.")
+                break
+        elif vyber == "5":
             pass
-        if vyber == "3":
+        elif vyber == "6":
             pass
-        if vyber == "4":
+        elif vyber == "7":
             pass
-        if vyber == "5":
+        elif vyber == "8":
             pass
-        if vyber == "6":
+        elif vyber == "9":
             pass
-        if vyber == "7":
-            pass
-        if vyber == "8":
-            pass
-        if vyber == "9":
-            pass
-        if vyber == "0":
+        elif vyber == "0":
             print("Konec")
             break
         else:
             print("Nebylo zadano cislo")
             continue
-
-zvol_akci()
--- temp.py
+++ temp.py
@@ -1,60 +1,61 @@
-
-
-import pandas as pd
-import matplotlib
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
 import menu
 import main
 
-data_path = 'data/klementinum.xlsx'
-data_sheet_name = 'data'
-temperature_data = pd.read_excel(data_path, sheet_name=data_sheet_name)
+class WeatherStatistics:
+    def __init__(self, dataset):
+        """
+        Inicializuje instanci třídy WeatherStatistics s datasetem.
+        param dataset: pandas DataFrame obsahující meteorologická data.
+        """
+        self.dataset = dataset
 
-class TemperatureAnalytics:
-    def __init__(self, data):
-        self.data = data
+    def highest_temperature(self, year):
+        """
+        Vrátí nejvyšší teplotu zaznamenanou v zadaném roce.
+        param year: Rok, pro který se má najít nejvyšší teplota.
+        return: Nejvyšší teplota v zadaném roce.
+        """
+        annual_data = self.dataset[self.dataset['rok'] == year]
+        highest_temp = annual_data['TMA'].max()
+        return highest_temp
 
-    def get_average_temperature(self, year_in):
-        year_in = input("Zadej rok pro vypocet rocniho prumeru")
-        yearly_data = self.data[self.data['rok'] == year_in]
-        return yearly_data['T-AVG'].mean()
+    def lowest_temperature(self, year):
+        """
+        Vrátí nejnižší teplotu zaznamenanou v zadaném roce.
+        param year: Rok, pro který se má najít nejnižší teplota.
+        return: Nejnižší teplota v zadaném roce.
+                """
+        annual_data = self.dataset[self.dataset['rok'] == year]
+        lowest_temp = annual_data['TMI'].min()
+        return lowest_temp
 
-    def get_max_temperature(self, year):
-        yearly_data = self.data[self.data['rok'] == year]
-        max_temp = yearly_data['TMA'].max()
-        date_of_max_temp = yearly_data[yearly_data['TMA'] == max_temp][['rok', 'měsíc', 'den']].iloc[0]
-        return max_temp, date_of_max_temp
+    def average_temperature(self, year):
+        """
+        Vypočítá a vrátí průměrnou teplotu pro zadaný rok.
+        param year: Rok, pro který se má vypočítat průměrná teplota.
+        return: Průměrná teplota v zadaném roce zaokrouhlená na dvě desetinná místa.
+                """
+        annual_data = self.dataset[self.dataset['rok'] == year]
+        return round(annual_data['T-AVG'].mean(), 2)
 
-    def get_min_temperature(self, year):
-        yearly_data = self.data[self.data['rok'] == year]
-        min_temp = yearly_data['TMI'].min()
-        date_of_min_temp = yearly_data[yearly_data['TMI'] == min_temp][['rok', 'měsíc', 'den']].iloc[0]
-        return min_temp, date_of_min_temp
+    def monthly_averages(self, year):
+        """
+        Vypočítá průměrné měsíční teploty pro zadaný rok.
+        param year: Rok, pro který se mají vypočítat měsíční průměry.
+        return: pandas Series obsahující průměrné teploty pro každý měsíc v zadaném roce, zaokrouhlené na dvě desetinná místa.
+              """
+        annual_data = self.dataset[self.dataset['rok'] == year]
+        monthly_avg = annual_data.groupby('měsíc')['T-AVG'].mean().round(2)
+        return monthly_avg
 
-    def get_monthly_averages(self, year):
-        yearly_data = self.data[self.data['rok'] == year]
-        return yearly_data.groupby('měsíc')['T-AVG'].mean()
-
-    def analyze_temperature_trends(self, start_year, end_year):
-        trend_data = self.data[(self.data['rok'] >= start_year) & (self.data['rok'] <= end_year)]
-        annual_average_temperatures = trend_data.groupby('rok')['T-AVG'].mean()
-        return annual_average_temperatures
-
-    def plot_avg_temp(self, start_year, end_year):
-        filtered_data = self.data[(self.data)["rok"] >= start_year & (self.data["rok"] <= end_year)]
-        annual_avg_temp = filtered_data.groupby("rok")["T-AVG"].mean()
-        from matplotlib import pyplot as plt
-        plt.figure(figsize=(10,6))
-        plt.plot(annual_avg_temp.index, annual_avg_temp.values, marker= "o", linestyle="-", color="b")
-        plt.title(f'Prumerne rocni teplotz mezi roky {start_year} a {end_year}')
-        plt.xlabel("rok")
-        plt.ylabel("prumerna teplota")
-        plt.grid(True)
-        plt.show()
-
-
-
-
-average_temp_2022 = TemperatureAnalytics.get_average_temperature()
+    def temperature_trends(self, start_year, end_year):
+        """
+        Analyzuje teplotní trendy mezi zadanými roky.
+        param start_year: Počáteční rok pro analýzu trendů.
+        param end_year: Koncový rok pro analýzu trendů.
+        return: pandas Series obsahující průměrné roční teploty pro každý rok v rozmezí, zaokrouhlené na dvě desetinná místa.
+                """
+        trend_data = self.dataset[(self.dataset['rok'] >= start_year) & (self.dataset['rok'] <= end_year)]
+        annual_avg = trend_data.groupby('rok')['T-AVG'].mean().round(2)
+        return annual_avg
 
